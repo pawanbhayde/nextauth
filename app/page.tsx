8427fc22f2c3c99333c9aec7beb3eb31e8709711
Pawan Bhayde <82137686+pawanbhayde@users.noreply.github.com>
2024-02-06 22:50:24 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <>
-      <p>Hello Auth </p>
-      <Button>Click Me</Button>
-    </>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authrntication service</p>
+
+        <div className="flex items-center justify-center">
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
